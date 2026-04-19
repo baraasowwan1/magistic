87362a3abe0cce7a7a0b44538804126d255a5613
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2026-04-19 22:35:32 +0300

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,12 @@
-import { RouterProvider } from "react-router";
-import { router } from "./routes";
+import { useEffect } from "react";
+import { useLocation } from "react-router";
 
-export default function App() {
-  return <RouterProvider router={router} />;
+export function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
 }
